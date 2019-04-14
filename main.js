100e185314b654e903fb83ef638b3e77fb0ab138
титаренко <kizuneworld@gmail.com>
2019-04-14 08:12:31 +0300

    test2

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 `use strict`;
 const searchI = document.querySelector('.search_input');
-const buttonSearch = document.querySelector('[data-button="search"]');
 
  function openInput(event){
-     console.log(event.target);
+
      if ( event.target.dataset.button === 'search') {
-         searchI.classList.add('is_active');
-         console.log(searchI.className);
-     } else {
-         searchI.classList.remove('is_active');
-         console.log(searchI.className);
+         searchI.style.transform="scale(1)";
+     } else if (null === event.target.closest('.search_input')) {
+         searchI.style.transform="scale(0)";
      }
  }
 
